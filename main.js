99d67f356896fa420ffdd308362a765c5559f3f5
Stephen Palazzo <stephencpalazzo@gmail.com>
2022-05-23 15:04:08 -0400

    Update: The method for generating the random pixel location

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const hint = document.getElementById('hint');
 const instructions = document.getElementById('instructions');
 const radioBtnDiv = document.getElementById('radioBtn');
 const restartBtn = document.getElementById('restart');
-const randWidth = Math.floor(Math.random() * (window.screen.width - 300) + 150);
-const randHeight = Math.floor(Math.random() * (window.screen.height - 300) + 150);
+const randWidth = Math.floor(Math.random() * window.innerWidth);
+const randHeight = Math.floor(Math.random() * window.innerHeight);
 let numOfClicks = 0;
 let gameOver = false;
 
@@ -18,11 +18,12 @@ document.addEventListener('click', e => {
 
     const clickedWidth = e.clientX; 
     const clickedHeight = e.clientY;
+    console.log(clickedWidth + " " + clickedHeight)
     numOfClicks++;
 
     if (Math.abs(randWidth - clickedWidth) < 50 && Math.abs(randHeight - clickedHeight) < 50) {
         gameOver = true;
-        hint.innerHTML = `You won in ${numOfClicks} number of clicks!<br> The location was ${randWidth}x${randHeight}.<br> Your location guessed was ${clickedWidth}x${clickedHeight}.`;
+        hint.innerHTML = `You won in ${numOfClicks} clicks!<br>The location was pixel ${randWidth}x${randHeight}.<br>Your winning guess was ${clickedWidth}x${clickedHeight}.`;
         radioBtnDiv.style.visibility = 'visible';
     } else if (Math.abs(randWidth - clickedWidth) < 100 && Math.abs(randHeight - clickedHeight) < 100) {
         hint.innerHTML = `ON FIRE!!!`;
