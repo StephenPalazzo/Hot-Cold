5c8f793e76765bc32c5efba7d3437734a3081e74
Stephen Palazzo <stephencpalazzo@gmail.com>
2022-06-24 15:53:48 -0400

    Updated Design

--- main.js
+++ main.js
@@ -9,41 +9,57 @@ let bestScore = localStorage.getItem('bestScore');
 let numOfClicks = 0;
 let gameOver = false;
 
-restartBtn.addEventListener('click', e => {
-    document.location.reload();
+restartBtn.addEventListener('click', (e) => {
+  document.location.reload();
 });
 
-document.addEventListener('click', e => {
-    if (gameOver) return;
+document.addEventListener('click', (e) => {
+  if (gameOver) return;
 
-    instructions.style.visibility = 'hidden';
+  instructions.style.visibility = 'hidden';
 
-    const clickedWidth = e.clientX; 
-    const clickedHeight = e.clientY;
-    numOfClicks++;
+  const clickedWidth = e.clientX;
+  const clickedHeight = e.clientY;
+  numOfClicks++;
 
-    if (Math.abs(randWidth - clickedWidth) < (window.innerWidth / 32) && Math.abs(randHeight - clickedHeight) < (window.innerHeight / 32)) {
-        gameOver = true;
-        hint.innerHTML = "";
-        gameStats.innerHTML = `The pixel location was ${randWidth}x${randHeight}.<br>Your winning guess was ${clickedWidth}x${clickedHeight}.`;
+  if (
+    Math.abs(randWidth - clickedWidth) < window.innerWidth / 32 &&
+    Math.abs(randHeight - clickedHeight) < window.innerHeight / 32
+  ) {
+    gameOver = true;
+    hint.innerHTML = '';
+    gameStats.style.visibility = 'visible';
+    gameStats.innerHTML = `Pixel Location: ${randWidth}x${randHeight}<br>Winning Guess: ${clickedWidth}x${clickedHeight}`;
 
-        if (bestScore == null || numOfClicks < bestScore) {
-            localStorage.setItem('bestScore', numOfClicks);
-            gameStats.innerHTML += `<br>New Best Score: ${numOfClicks}`;
-        } else {
-            gameStats.innerHTML += `<br>Score: ${numOfClicks}<br>Previous Best Score: ${bestScore}`;
-        }
-
-        radioBtnDiv.style.visibility = 'visible';
-    } else if (Math.abs(randWidth - clickedWidth) < (window.innerWidth / 16) && Math.abs(randHeight - clickedHeight) < (window.innerHeight / 16)) {
-        hint.innerHTML = `ON FIRE!!!`;
-    } else if (Math.abs(randWidth - clickedWidth) < (window.innerWidth / 8) && Math.abs(randHeight - clickedHeight) < (window.innerHeight / 8)) {
-        hint.innerHTML = `HOT!`;
-    } else if (Math.abs(randWidth - clickedWidth) < (window.innerWidth / 4) && Math.abs(randHeight - clickedHeight) < (window.innerHeight / 4)) {
-        hint.innerHTML = `Warm`;
-    } else if (Math.abs(randWidth - clickedWidth) < (window.innerWidth / 2) && Math.abs(randHeight - clickedHeight) < (window.innerHeight / 2)) {
-        hint.innerHTML = `Cold`;
+    if (bestScore == null || numOfClicks < bestScore) {
+      localStorage.setItem('bestScore', numOfClicks);
+      gameStats.innerHTML += `<br>New Best Score: ${numOfClicks}`;
     } else {
-        hint.innerHTML = `Freezing!`;
+      gameStats.innerHTML += `<br>Score: ${numOfClicks}<br>Previous Best Score: ${bestScore}`;
     }
+
+    radioBtnDiv.style.visibility = 'visible';
+  } else if (
+    Math.abs(randWidth - clickedWidth) < window.innerWidth / 16 &&
+    Math.abs(randHeight - clickedHeight) < window.innerHeight / 16
+  ) {
+    hint.innerHTML = `ON FIRE!!!`;
+  } else if (
+    Math.abs(randWidth - clickedWidth) < window.innerWidth / 8 &&
+    Math.abs(randHeight - clickedHeight) < window.innerHeight / 8
+  ) {
+    hint.innerHTML = `HOT!`;
+  } else if (
+    Math.abs(randWidth - clickedWidth) < window.innerWidth / 4 &&
+    Math.abs(randHeight - clickedHeight) < window.innerHeight / 4
+  ) {
+    hint.innerHTML = `Warm`;
+  } else if (
+    Math.abs(randWidth - clickedWidth) < window.innerWidth / 2 &&
+    Math.abs(randHeight - clickedHeight) < window.innerHeight / 2
+  ) {
+    hint.innerHTML = `Cold`;
+  } else {
+    hint.innerHTML = `Freezing!`;
+  }
 });
